5e869501307a5d33a2ca318bb513fb0eb5d83be0
yajinni <yajinni@gmail.com>
2026-07-13 16:08:21 -0400

    style: load provider integration controls

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./updater.css";
+import "./provider.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
